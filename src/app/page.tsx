166a0159fd6508fb880ed19825f6bd2f944c36bf
JogadorHungria <joilsonhungrianv@gmail.com>
2024-03-05 22:28:26 -0300

    feat: adjusting in color footer and header and fixed header in top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,84 @@ export default function Home() {
   return (
     <>
       <main>
-        <h1>Main</h1>
+        <div>
+          <h1>Main</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, voluptatum repellat harum deserunt veritatis quasi cupiditate dolore vero provident sint, unde quisquam in, corrupti iure odio beatae minus modi fugit.
+          </p>
+        </div>
       </main>
     </>
 
